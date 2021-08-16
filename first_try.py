2f9b01738ed36546e960838357a1b39bd1120a02
Viktor Haliabar <vhaliabar@yahoo.com>
2021-08-16 14:48:50 +0300

    Add sys.argv for a convinient way to include urls from command line

--- first_try.py
+++ first_try.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
-import requests
 import re
-#import sys
-
-
+import sys
 from urllib.request import Request, urlopen
 
-req = Request('https://sec.report/Document/0001193125-21-072571/', headers={'User-Agent': 'Mozilla/5.0'})
+req = Request(sys.argv[1], headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read().decode()
 #print(webpage[500:1000])
+
 pattern_shares = r'[s|S]hares \d+,\d+,\d+|\d+,\d+,\d+ [s|S]hares'
 result_shares = re.findall(pattern_shares, webpage)
 print("Found results with 'Number + Shares' : ", result_shares[:10])
 
-print('-----------------------------------------------------------------------------', '\n')
+print('\n','-----------------------------------------------------------------------------')
 pattern_IPO = r'(\$10.00)'
 result_IPO = re.findall(pattern_IPO, webpage)
 print("Result in 'IPO' - $10 : ", result_IPO[:10])
 
-print('-----------------------------------------------------------------------------', '\n')
+print('\n','-----------------------------------------------------------------------------')
 pattern_IPO_price = r'(\$\d+\.\d+|\d+\.\d+\$)'
 result_IPO_price = re.findall(pattern_IPO_price, webpage)
 print("Result in 'IPO price' : ", result_IPO_price[:10])
